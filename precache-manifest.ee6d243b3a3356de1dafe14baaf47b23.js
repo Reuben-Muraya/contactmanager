8d25e332af2f66626c192e1323d62fa22621d89f
Reuben Muraya <reubenmuraya4@gmail.com>
2020-07-14 14:55:21 +0300

    Updates

---
+++ precache-manifest.ee6d243b3a3356de1dafe14baaf47b23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6093ede46cb9ba27c32bfe2d54654f8",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "7e4f261e09240613355a",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "dce7794967d649ce8c25",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "7e4f261e09240613355a",
+    "url": "/contactmanager/static/js/2.d06b3187.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.d06b3187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dce7794967d649ce8c25",
+    "url": "/contactmanager/static/js/main.ef0d55a9.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);
